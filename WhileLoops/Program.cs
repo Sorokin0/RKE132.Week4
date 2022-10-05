8a2b2f9a6a507ac24b3d0a8805f7b1ae00ae8588
Sorokin0 <s.aleksander.99.09@gmail.com>
2022-10-05 22:07:22 +0300

    Week 4 project

---
+++ WhileLoops/Program.cs
@@ -0,0 +1,24 @@
+﻿
+Random rnd = new Random();
+int cpuRandom;
+bool loopAtive = true;
+
+while (loopAtive)
+{
+    cpuRandom = rnd.Next(1, 4);
+    Console.WriteLine("Make a gues. Enter a number 1-3:");
+
+    int userNumber = Int32.Parse(Console.ReadLine());
+
+    if (userNumber == cpuRandom)
+    {
+        Console.WriteLine("Congratulations!");
+        // loopAtive = false;
+        break;
+    } else
+    {
+        Console.WriteLine("No, try again!");
+    }    
+}
+
+Console.WriteLine("You win!");
